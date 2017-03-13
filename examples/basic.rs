18ebb26405c32bf905124a1685d9369e527dccc0
Danilo Bargen <mail@dbrgn.ch>
2017-03-13 17:55:52 +0100

    Extract parse_segment_data function

--- examples/basic.rs
+++ examples/basic.rs
@@ -9,8 +9,10 @@ use std::process::exit;
 use svg2polylines::Polyline;
 
 fn main() {
+    // Logging
     env_logger::init().expect("Could not initialize env logger");
 
+    // Argument parsing
     let args: Vec<_> = env::args().collect();
     match args.len() {
         2 => {},
@@ -20,14 +22,18 @@ fn main() {
         },
     };
 
+    // Load file
     let mut file = fs::File::open(&args[1]).unwrap();
     let mut s = String::new();
     file.read_to_string(&mut s).unwrap();
 
+    // Parse data
     let polylines: Vec<Polyline> = svg2polylines::parse(&s).unwrap_or_else(|e| {
         println!("Error: {}", e);
         exit(2);
     });
+
+    // Print data
     println!("Found {} polylines.", polylines.len());
     for line in polylines {
         println!("- {:?}", line);
--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,61 @@
 extern crate svgparser;
 
 use std::str;
+use std::mem;
 
 use svgparser::{svg, path, Stream};
-use svgparser::path::SegmentData::{MoveTo, LineTo, HorizontalLineTo, VerticalLineTo};
+use svgparser::path::SegmentData::{self, MoveTo, LineTo, HorizontalLineTo, VerticalLineTo};
 
 pub type CoordinatePair = (f64, f64);
 pub type Polyline = Vec<CoordinatePair>;
+
+
+struct CurrentLine {
+    line: Polyline,
+}
+
+impl CurrentLine {
+    fn new() -> Self {
+        CurrentLine { line: Polyline::new() }
+    }
+
+    /// Add a CoordinatePair to the internal polyline.
+    fn add(&mut self, pair: CoordinatePair) {
+        self.line.push(pair);
+    }
+
+    /// A polyline is only valid if it has more than 1 CoordinatePair.
+    fn is_valid(&self) -> bool {
+        self.line.len() > 1
+    }
+
+    /// Replace the internal polyline with a new instance and return the
+    /// previously stored polyline.
+    fn finish(&mut self) -> Polyline {
+        let mut tmp = Polyline::new();
+        mem::swap(&mut self.line, &mut tmp);
+        tmp
+    }
+}
+
+fn parse_segment_data(data: &SegmentData,
+                      current_line: &mut CurrentLine,
+                      lines: &mut Vec<Polyline>) {
+    match data {
+        &MoveTo { x: x, y: y } => {
+            if current_line.is_valid() {
+                lines.push(current_line.finish());
+            }
+            current_line.add((x, y));
+        },
+        &LineTo { x: x, y: y } => {
+            current_line.add((x, y));
+        },
+        d @ _ => {
+            println!("Unsupported segment data: {:?}", d);
+        }
+    }
+}
 
 fn parse_path(data: Stream) -> Vec<Polyline> {
     debug!("New path");
@@ -15,28 +64,14 @@ fn parse_path(data: Stream) -> Vec<Polyline> {
     let mut lines = Vec::new();
 
     let mut p = path::Tokenizer::new(data);
-    let mut line = Polyline::new();
+    let mut line = CurrentLine::new();
     loop {
         match p.parse_next() {
             Ok(segment_token) => {
                 match segment_token {
                     path::SegmentToken::Segment(segment) => {
                         debug!("  Segment data: {:?}", segment.data);
-                        match segment.data {
-                            MoveTo { x: x, y: y } => {
-                                if line.len() > 1 {
-                                    lines.push(line);
-                                }
-                                line = Polyline::new();
-                                line.push((x, y));
-                            },
-                            LineTo { x: x, y: y } => {
-                                line.push((x, y));
-                            },
-                            d @ _ => {
-                                println!("Unsupported segment data: {:?}", d);
-                            }
-                        }
+                        parse_segment_data(&segment.data, &mut line, &mut lines);
                     },
                     path::SegmentToken::EndOfStream => break,
                 }
