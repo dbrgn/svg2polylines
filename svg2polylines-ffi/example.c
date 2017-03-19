5c68f650cf0e380f090fcabfc29e3cc4511db215
Danilo Bargen <mail@dbrgn.ch>
2017-03-19 19:44:38 +0100

    Properly pass data to C code

--- svg2polylines-ffi/example.c
+++ svg2polylines-ffi/example.c
@@ -13,23 +13,29 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-struct CoordinatePair {
+typedef struct CoordinatePair {
     double x;
     double y;
-};
+} CoordinatePair;
+
+typedef struct Polyline {
+    CoordinatePair* ptr;
+    size_t len;
+} Polyline;
 
 uint8_t svg_str_to_polylines(char* svg,
-                             struct CoordinatePair** out_vec,
+                             Polyline** out_vec,
                              size_t* out_vec_len);
 
 int main() {
     char* input = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?><svg xmlns=\"http://www.w3.org/2000/svg\" id=\"svg8\" version=\"1.1\" viewBox=\"0 0 140.1311 56.978192\" height=\"56.978191mm\" width=\"140.1311mm\"><g transform=\"translate(-24.666516,-30.77247)\" id=\"layer1\"><path id=\"path4485\" d=\"m 70.303571,34.306548 -40.443453,44.601188 65.767856,4.91369 z\" style=\"fill:none;fill-rule:evenodd;stroke:#000000;stroke-width:0.26458332px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1\" /><path id=\"path4487\" d=\"m 113.01488,35.818452 h 40.44345 l -39.6875,49.514881 h 40.06548\" style=\"fill:none;fill-rule:evenodd;stroke:#000000;stroke-width:0.26458332px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1\" /></g></svg>";
-    struct CoordinatePair** out_vec = NULL;
+    Polyline* out_vec = NULL;
     size_t out_vec_len = 0;
-    uint8_t err = svg_str_to_polylines(input, out_vec, &out_vec_len);
+    uint8_t err = svg_str_to_polylines(input, &out_vec, &out_vec_len);
     if (err > 0) {
         printf("Error: Return code %d", err);
         exit(err);
     }
     printf("Found %zu polylines!\n", out_vec_len);
+    printf("Out vec address: %p\n", out_vec);
 }
--- svg2polylines-ffi/src/lib.rs
+++ svg2polylines-ffi/src/lib.rs
@@ -4,16 +4,23 @@ extern crate libc;
 extern crate svg2polylines;
 
 use std::ffi::CStr;
-use std::mem;
 
-use libc::{c_char};
+use libc::{c_char, size_t};
 use svg2polylines::{CoordinatePair, parse};
+
+/// Structure that contains a pointer to the coordinate pairs as well as the
+/// number of coordinate pairs. It is only used for C interop.
+#[repr(C)]
+pub struct Polyline {
+    ptr: *const CoordinatePair,
+    len: size_t,
+}
 
 #[no_mangle]
 pub extern fn svg_str_to_polylines(
     svg: *const c_char,
-    out_vec: *const *const CoordinatePair,
-    out_vec_len: *mut usize,
+    out_vec: *mut *mut Polyline,
+    out_vec_len: *mut size_t,
 ) -> u8 {
 
     // Convert C string to Rust string
@@ -25,17 +32,26 @@ pub extern fn svg_str_to_polylines(
 
     // Process
     match parse(r_str) {
-        Ok(mut vec) => {
+        Ok(vec) => {
             println!("Done!");
-            vec.shrink_to_fit();
-            println!("Rust says: Found {} polylines", vec.len());
-//            out_vec = vec.map(|v| v.as_ptr()).as_ptr();
-            unsafe { *out_vec_len = vec.len(); }
-            mem::forget(vec);
+
+            // Convert `Vec<Vec<CoordinatePair>>` to `Vec<Polyline>`
+            let mut tmp_vec: Vec<Polyline> = vec.iter().map(|v| Polyline {
+                ptr: v.as_ptr(),
+                len: v.len(),
+            }).collect();
+            tmp_vec.shrink_to_fit();
+            assert!(tmp_vec.len() == tmp_vec.capacity());
+
+            // Return number of polylines
+            unsafe { *out_vec_len = tmp_vec.len(); }
+
+            // Move data to heap and return pointer to it
+            let boxed = Box::new(tmp_vec);
+            unsafe { *out_vec = Box::into_raw(boxed) as *mut Polyline; }
+
             0
         },
-        Err(e) => {
-            1
-        }
+        Err(_) => 1
     }
 }
