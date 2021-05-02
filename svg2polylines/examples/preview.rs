2b9018b2831bd8bdd32f3944d68e5d6d89a7be42
Danilo Bargen <mail@dbrgn.ch>
2021-05-02 23:11:55 +0200

    Apply rustfmt

--- svg2polylines/examples/preview.rs
+++ svg2polylines/examples/preview.rs
@@ -5,9 +5,9 @@ use std::fs;
 use std::io::Read;
 use std::process::exit;
 
-use drag_controller::{DragController, Drag};
-use piston_window::{PistonWindow, WindowSettings, OpenGL, Transformed, clear, line};
+use drag_controller::{Drag, DragController};
 use piston_window::math::Matrix2d;
+use piston_window::{clear, line, OpenGL, PistonWindow, Transformed, WindowSettings};
 use svg2polylines::{self, Polyline};
 
 fn main() {
@@ -17,11 +17,11 @@ fn main() {
     // Argument parsing
     let args: Vec<_> = env::args().collect();
     match args.len() {
-        2 => {},
+        2 => {}
         _ => {
             println!("Usage: {} <path/to/file.svg>", args[0]);
             exit(1);
-        },
+        }
     };
 
     // Load file
@@ -50,8 +50,7 @@ fn main() {
     let black = [0.0, 0.0, 0.0, 1.0];
     let radius = 1.0;
     let mut drag = DragController::new();
-    let mut translate: Matrix2d = [[1.0, 0.0, 0.0],
-                                   [0.0, 1.0, 0.0]];
+    let mut translate: Matrix2d = [[1.0, 0.0, 0.0], [0.0, 1.0, 0.0]];
     let mut translate_tmp: Matrix2d = translate.clone();
     let mut translate_start = None;
     while let Some(e) = window.next() {
@@ -60,18 +59,18 @@ fn main() {
                 Drag::Start(x, y) => {
                     translate_start = Some((x, y));
                     true
-                },
+                }
                 Drag::Move(x, y) => {
                     let start_x = translate_start.unwrap().0;
                     let start_y = translate_start.unwrap().1;
                     translate_tmp = translate.trans(x - start_x, y - start_y);
                     true
-                },
+                }
                 Drag::End(..) => {
                     translate_start = None;
                     translate = translate_tmp;
                     false
-                },
+                }
                 // Continue dragging when receiving focus.
                 Drag::Interrupt => true,
             }
@@ -80,11 +79,15 @@ fn main() {
             clear([1.0; 4], g);
             for polyline in &polylines {
                 for pair in polyline.windows(2) {
-                    line(black,
-                         radius,
-                         [pair[0].x, pair[0].y, pair[1].x, pair[1].y],
-                         c.transform.append_transform(translate_tmp).scale(fscale, fscale),
-                         g);
+                    line(
+                        black,
+                        radius,
+                        [pair[0].x, pair[0].y, pair[1].x, pair[1].y],
+                        c.transform
+                            .append_transform(translate_tmp)
+                            .scale(fscale, fscale),
+                        g,
+                    );
                 }
             }
         });
