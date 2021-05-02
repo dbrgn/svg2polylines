1a067d595b9228fa17c62d869339dfab1f492fa5
Danilo Bargen <mail@dbrgn.ch>
2021-05-02 23:33:48 +0200

    FFI: Mark both functions as unsafe
    Since the unsafety depends on parameters passed to the function, we
    cannot guarantee its safety. Therefore, mark both as unsafe.

--- svg2polylines-ffi/src/lib.rs
+++ svg2polylines-ffi/src/lib.rs
@@ -15,15 +15,34 @@ pub struct Polyline {
     len: size_t,
 }
 
+/// Convert the specified SVG string to an array of polylines.
+///
+/// The two pointers `polylines` and `polylines_len` are out parameters.
+/// Initialize them like this:
+///
+/// ```c
+/// Polyline* polylines = NULL;
+/// size_t polylines_len = 0;
+/// ```
+///
+/// Pass them in like this:
+///
+/// ```c
+/// uint8_t err = svg_str_to_polylines(input, &polylines, &polylines_len);
+/// ```
+///
+/// # Safety
+///
+/// The `svg` pointer must point to a valid C-style 0-terminated string.
 #[no_mangle]
-pub extern fn svg_str_to_polylines(
+pub unsafe extern fn svg_str_to_polylines(
     svg: *const c_char,
     polylines: *mut *mut Polyline,
     polylines_len: *mut size_t,
 ) -> u8 {
 
     // Convert C string to Rust string
-    let c_str = unsafe {
+    let c_str = {
         assert!(!svg.is_null());
         CStr::from_ptr(svg)
     };
@@ -46,10 +65,10 @@ pub extern fn svg_str_to_polylines(
             assert!(tmp_vec.len() == tmp_vec.capacity());
 
             // Return number of polylines
-            unsafe { *polylines_len = tmp_vec.len() as size_t; }
+            *polylines_len = tmp_vec.len() as size_t;
 
             // Return pointer to data
-            unsafe { *polylines = tmp_vec.as_mut_ptr(); }
+            *polylines = tmp_vec.as_mut_ptr();
 
             // Prevent memory from being deallocated
             mem::forget(tmp_vec);
@@ -60,11 +79,15 @@ pub extern fn svg_str_to_polylines(
     }
 }
 
+/// Free the specified `polyline_len` polylines.
+///
+/// # Safety
+///
+/// The user must be sure that the raw pointer is still valid and points to a
+/// polyline array previously allocated by `svg_str_to_polylines`.
 #[no_mangle]
-pub extern fn free_polylines(polylines: *mut Polyline, polylines_len: size_t) {
-    unsafe {
-        for p in Vec::from_raw_parts(polylines, polylines_len as usize, polylines_len as usize) {
-            Vec::from_raw_parts(p.ptr, p.len as usize, p.len as usize);
-        }
+pub unsafe extern fn free_polylines(polylines: *mut Polyline, polylines_len: size_t) {
+    for p in Vec::from_raw_parts(polylines, polylines_len as usize, polylines_len as usize) {
+        Vec::from_raw_parts(p.ptr, p.len as usize, p.len as usize);
     }
 }
